5b83c3762473671032b5688286c23305941ca463
abezuska <abezuska@gmail.com>
2017-01-02 12:56:33 -0500

    add config object, add more templates

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
+var config = {
+  /*
+    urls - currently this program only woths with ludumdare.com game pages
+  */
+  urls : [
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=34387",
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=35711",
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=40673",
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=57618",
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=58120",
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=58187",
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=63943",
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=64869",
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=8310",
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=90468",
+    "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=92205"
+  ],
+  /*
+    ordering options
+    "default" - games will be presented in order of the urls in the array
+    "alpha" - games will be in alphabetical order by title
+  */
+  ordering : "alpha",
+  template : "./templates/ludumdare-bootstrap.html",
+  outputFile : "./output/blog-post.html"
+};
+
+
+/* Application */
 
 var fs = require("fs");
 var request = require("request");
 var cheerio = require("cheerio");
 var handlebars = require("handlebars");
 
-// urls of the 'Louisville Makes Games' community's LD37 games!
-urls = [
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=34387",
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=35711",
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=40673",
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=57618",
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=58120",
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=58187",
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=63943",
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=64869",
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=8310",
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=90468",
-  "http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=92205"
-];
-
 var json = [];
 var requests = 0;
-for (var i = 0; i < urls.length; i++) {
-  var currentUrl = urls[i];
+for (var i = 0; i < config.urls.length; i++) {
+  var currentUrl = config.urls[i];
   (function(i){
     request(currentUrl,
       function(err, res, body){
@@ -31,7 +45,7 @@ for (var i = 0; i < urls.length; i++) {
           requests++;
           var $ = cheerio.load(body);
           var game = {
-            url: urls[i],
+            url: config.urls[i],
             title : ldGet.title($),
             type : ldGet.type($),
             author : ldGet.author($),
@@ -43,9 +57,10 @@ for (var i = 0; i < urls.length; i++) {
           json.push(game);
           requests--;
           if (requests === 0){
-            json = sortArrayObjects(json, "title");
-
-            fs.writeFile("index.html", renderFromExternalTemplate("./template.html", json), function(err) {
+            if (config.ordering === "alpha"){
+              json = sortArrayObjects(json, "title");
+            }
+            fs.writeFile(config.outputFile, renderFromExternalTemplate(config.template, json), function(err) {
               if(err) {
                 return console.log(err);
               }
@@ -74,6 +89,12 @@ for (var i = 0; i < urls.length; i++) {
     return str.replace(new RegExp(find, 'g'), replace);
   }
 
+
+/*
+  Functions that handle the cheerio(jquery) selectors for grabbing various
+  content from ludumdare.com game pages
+  Needs to be seperated into new library
+*/
   var ldGet = {
     title : function ($) {
       return $("#compo-body").find("h2").first().text();
